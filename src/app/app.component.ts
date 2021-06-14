75fe825009b56589d48ff5a2678085e29eb04e47
Jack Bennett <jabennetto@yahoo.co.uk>
2021-06-14 11:14:17 +0100

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-app';
+  quizData: any = [];
+  public quizName = "";
+
+  constructor(public httpService: HttpService) { }
+
+  ngOnInit() {
+    this.getQuizData();
+  }
+
+  getQuizData() {
+    this.httpService.get("quiz/getQuizzes").subscribe((rs: any) => {
+      this.quizData = rs;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  newQuiz() {
+    this.httpService.post("quiz/createQuiz", { QuizName: this.quizName, Status: 1 }).subscribe((rs: any) => {
+      if (rs == 1) {
+        this.getQuizData();
+        console.log("Success")
+      } else {
+        console.log("Error")
+      }
+    }, (err: any) => {
+      console.log("Error")
+    });
+  }
 }
